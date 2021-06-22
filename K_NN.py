577219405e989f7b29c37c0cf5e0ab94ce93df9e
SangND <sanga1k30@gmail.com>
2021-06-23 02:39:27 +0700

    add KNN

---
+++ K_NN.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import preprocessing, neighbors, model_selection
+import pandas as pd
+
+dt = pd.read_csv('breast-cancer-wisconsin.data')
+dt.replace('?', -99999, inplace=True)
+dt.drop(['id'], 1, inplace=True)
+
+X = np.array(dt.drop(['class'], 1))
+y = np.array(dt['class'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+model = neighbors.KNeighborsClassifier()
+model.fit(X_train, y_train)
+
+acc = model.score(X_test, y_test)
+
+print(acc)
+
+example_measure = np.array([4,2,1,1,1,2,3,2,1])
+example_measure = example_measure.reshape(1, -1)
+prediction = model.predict(example_measure)
+
+print(prediction)
